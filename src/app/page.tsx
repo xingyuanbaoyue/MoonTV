a9f799479c8430eaab40075a411ca2e94f685e28
白月 日和（しらつき ひより） <xiaxiqiuri@gmail.com>
2025-09-25 22:53:33 +0900

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -206,15 +206,34 @@ function HomeClient() {
             <>
               {/* 继续观看 */}
               <ContinueWatching />
-              {/* 下载按钮：apk，Windows客户端github */}
-              <div className='mb-6 px-1'>
-                <Link
-                  href='https://github.com/xingyuanbaoyue/MoonTV/releases/tag/v1.0.0'
-                  target='_blank'
-                  rel='noopener noreferrer'
+              {/* 下载按钮 */}
+              <div className='mb-6 px-1 space-x-3 flex flex-wrap gap-2'>
+                <a
+                  href='/v1.0.0.apk'
+                  download='MoonTV-v1.0.0.apk'
+                  className='inline-flex items-center rounded-lg bg-gray-400 px-3 py-2 text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 text-sm relative group'
+                  title='不推荐使用APK版本'
+                >
+                  <span>APK下载</span>
+                  <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-2 py-0.5'>不推荐</span>
+                </a>
+                <a
+                  href='/win32-x64.exe'
+                  download='MoonTV-Windows-x64.exe'
                   className='inline-flex items-center rounded-lg bg-blue-600 px-3 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 text-sm'
                 >
-                  apk，Windows客户端github
+                  Windows客户端
+                </a>
+                <Link
+                  href='https://github.com/xingyuanbaoyue/MoonTV'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='inline-flex items-center rounded-lg bg-gray-800 px-3 py-2 text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 text-sm'
+                >
+                  <svg className='w-4 h-4 mr-1' viewBox='0 0 24 24' fill='currentColor'>
+                    <path d='M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12'/>
+                  </svg>
+                  GitHub
                 </Link>
               </div>
 
